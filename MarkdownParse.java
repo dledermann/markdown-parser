5ba08ba26ae7ca5570e83ee27985d0c24c430621
dledermann <103291767+dledermann@users.noreply.github.com>
2022-04-22 16:49:00 -0700

    Original

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,18 +16,6 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(openBracket==-1){
-                break;
-            }
-            if(closeBracket==-1){
-                break;
-            }
-            if(openParen==-1){
-                break;
-            }
-            if(closeParen==-1){
-                break;
-            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
